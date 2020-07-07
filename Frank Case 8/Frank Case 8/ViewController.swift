6787f13b027a69069a37bb07d25d904b67b8f626
Frank Yan <frank.v.yan@gmail.com>
2020-07-07 18:56:23 -0400

    initial commit

---
+++ Frank Case 8/Frank Case 8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Frank Case 8
+//
+//  Created by Chenguo Yan on 2020-07-07.
+//  Copyright © 2020 Chenguo Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
